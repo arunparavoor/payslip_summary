e97a96aeb536efe200389439b2e3e6a68e41a1fd
arunparavoor <41280036+arunparavoor@users.noreply.github.com>
2019-06-30 17:21:48 +0400

    images

--- payslip_summary/__manifest__.py
+++ payslip_summary/__manifest__.py
@@ -16,7 +16,8 @@
     ],
     'data': [       
         'wizard/pay_slip_summary_view.xml'
-    ],	
+    ],
+    'images': ['static/description/banner.png'],
     'installable': True,
     'auto_install': False,
 }
